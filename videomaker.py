0daae51b534c3149732cff4916c83fa018f5ae09
Ibrahim Khaled <ikhaled@ulb.ac.be>
2020-04-05 22:44:55 +0200

    trying to download vid using url

--- videomaker.py
+++ videomaker.py
@@ -1,8 +1,10 @@
 
 #!/usr/bin/python3
+import os
 import json
 import numpy as np
 import pandas as pd
+import wget
 
 #Loading data
 with open('dataVideo.json','r') as f:
@@ -10,14 +12,22 @@ with open('dataVideo.json','r') as f:
 
 #Manipulate data
 df = pd.DataFrame.from_dict(videos_dict)
-df_shorter = df.drop(columns=['url','timeCreated','videoUsed','videoUsedDate'])
-columns_name = ['id','commentCount','likeCount','playCount','shareCount']
-df_shorter = df_shorter.head()
-df_shorter = df_shorter.reindex(columns=columns_name)
+#df_shorter = df.drop(columns=['url','timeCreated','videoUsed','videoUsedDate'])
+#columns_name = ['id','commentCount','likeCount','playCount','shareCount']
+#df_shorter = df_shorter.head()
+#df_shorter = df_shorter.reindex(columns=columns_name)
 
-dataNorm=((df_shorter-df_shorter.min())/(df_shorter.max()-df_shorter.min()))*1
-dataNorm["id"]=df_shorter["id"]
-print(dataNorm)
+#dataNorm=((df_shorter-df_shorter.min())/(df_shorter.max()-df_shorter.min()))*1
+#dataNorm["id"]=df_shorter["id"]
+#print(dataNorm)
 #TODO: calculate a score for all videos in order to select the x best videos
 
 #Download videos localy
+path = os.getcwd()
+urls = df['url'].head()
+#vid_dl = []
+#for u in urls:
+#    vid_dl.append(wget.download(u))
+vid = wget.download(urls[0],path)
+print(vid)
+#print(vid_dl)
